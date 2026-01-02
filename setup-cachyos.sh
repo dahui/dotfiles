1473f097507f6c649662f5804ac8fa8f71539af2
Jeff Hagadorn <jeff@aletheia.io>
2026-01-01 23:15:44 -0800

    Add cachy setup script

---
+++ setup-cachyos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing packages..."
+paru -Sy --repo --skipreview stow hhd hhd-ui acpi_call \
+    vivaldi ghostty linux-bazzite-bin cachyos-gaming-meta \
+    vscode bazaar protonup-qt protonplus sbctl ryzenadj dkms \
+    nerd-fonts wireguard-tools minio-client
+
+echo "configuring secure boot..."
+sudo sbctl create-keys
+sudo sbctl enroll-keys --microsoft
+sudo sbctl-batch-sign
+
+echo "Setting up dkms for secure boot..."
+sudo cp cachy-configs/etc/dkms/* /etc/dkms/
+
+echo "Installing dkms kernel modules..."
+paru -Sy --repo --skipreview xone-dkms ryzen_smu-dkms-git
+
+echo "Setting up module configs..."
+sudo cp -a cachy-configs/etc/modprobe.d/* /etc/modprobe.d
+sudo chown root:root /etc/modprobe.d/*
+
+echo "done!"
