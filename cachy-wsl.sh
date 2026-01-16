562a19991fd6e47d3dc37c6396fbbea0e87c0d32
Jeff Hagadorn <jeff@aletheia.io>
2026-01-15 17:57:14 -0800

    update cachy script

--- cachy-wsl.sh
+++ cachy-wsl.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-pacman -Sy --noconfirm sudo curl wget
+# Get some basic tools that are missing.
+#
+pacman -Sy --noconfirm sudo curl wget which base-devel
 
+# Install the CachyOS repositories.
+#
+cd /tmp
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
-tar xvf cachyos-repo.tar.xz && cd cachyos-repo\
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
 
-pacman -Sy --noconfirm cachyos-hooks \
-      cachyos-keyring \
-      cachyos-mirrorlist \
-      cachyos-v3-mirrorlist \
-      cachyos-v4-mirrorlist
-      cachyos-rate-mirrors \
+# cachyos-rate-mirrors doesn't get installed with the above scripts for whatever reason,
+# so install and run it.
+#
+sudo pacman -Sy --noconfirm cachyos-rate-mirrors
+sudo cachyos-rate-mirrors
+
+# Install core CachyOS packages, as described in their official
+# installer configs, minus an X environment and other unneeded
+# tools for WSL such as bluetooth control, wireless card tools, 
+# kernels, etc. 
+#
+sudo pacman -Sy --noconfirm cachyos-hooks \
       chwd \
       cachyos-hello \
       cachyos-kernel-manager \
@@ -79,4 +90,6 @@ pacman -Sy --noconfirm cachyos-hooks \
       vim \
       amd-ucode \
       intel-ucode \
+      openssh \
+      code
 
